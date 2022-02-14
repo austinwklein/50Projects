923fbf111f0134d213f40e4f06c7345cab674578
Austin <54128045+austinwklein@users.noreply.github.com>
2022-02-15 08:42:20 +1000

    JS : Day 9 - Sound Board Script plays sounds when button is pushed - Complete

--- Day 9/script.js
+++ Day 9/script.js
@@ -2,12 +2,24 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
-    console.log(btn.classList)
     btn.classList.add('btn')
-
-    console.log(btn.classList)
 
     btn.innerText = sound
 
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
     document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
