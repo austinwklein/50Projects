0d7d373109223bd917e08c27c625404a6b1ee7b3
Austin <54128045+austinwklein@users.noreply.github.com>
2022-02-15 10:50:37 +1000

    JS : Day 11 - Event KeyCodes Scripting INW - Stopping point

---
+++ Day 11 - Event KeyCodes/script.js
@@ -0,0 +1,11 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    console.log(e)
+})
+
+
+// FROM THE CONSOLE (Target Values)
+// code - code
+// key - key
+// keyCode - keyCode
